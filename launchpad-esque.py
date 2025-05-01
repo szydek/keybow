e8310ea66a0edea76ecb31e8590caa6a5c266ab5
Steven Zydek <37217452+szydek@users.noreply.github.com>
2025-04-30 20:00:33 -0500

    Update launchpad-esque.py

--- launchpad-esque.py
+++ launchpad-esque.py
@@ -1,3 +1,4 @@
+
 # Keybow 2040 MIDI controller with LED feedback from Ableton
 # copy to code.py to run
 # this version also allows for different color feedback on keys second column from the right (C0, C#0, D0, D#0)
@@ -33,7 +34,7 @@ ctl_rgb = (255,165,0) # will be color for future control button
 feedback_rgb = (0, 0, 255)  # Different color for feedback (e.g., blue)
 
 # MIDI note and velocity settings
-start_note = 36  # Starting MIDI note for Keybow (C0)
+start_note = 0  # Starting MIDI note for Keybow (C-2) - set low as possible
 velocity = 127   # Note on velocity
 
 # Track LED states for each key
@@ -61,61 +62,61 @@ def midi_feedback():
         if isinstance(msg, NoteOn):
             note = msg.note
             vel = msg.velocity
-            if note == 24:  # Check for C0 (note 24)
+            # if note == 24:  # Check for C0 (note 24)
+            #     if vel > 0:
+            #         keys[11].set_led(*feedback_rgb)  # Set key 11 (index 11) to a different color
+            #     else:
+            #         keys[11].set_led(0, 0, 0)  # Turn off key 11 LED
+            # elif note == 25:  # C#0
+            #     if vel > 0:
+            #         keys[10].set_led(*feedback_rgb)
+            #     else:
+            #         keys[10].set_led(0, 0, 0)
+            # elif note == 26:  # D0
+            #     if vel > 0:
+            #         keys[9].set_led(*feedback_rgb)
+            #     else:
+            #         keys[9].set_led(0, 0, 0)
+            # elif note == 27:  # D#0
+            #     if vel > 0:
+            #         keys[8].set_led(*feedback_rgb)
+            #     else:
+            #         keys[8].set_led(0, 0, 0)
+            # else:
+            # Handle other NoteOn messages normally for mapping
+            key_index = note - start_note
+            if 0 <= key_index < len(keys):  # Setup LED color per mapped key
                 if vel > 0:
-                    keys[11].set_led(*feedback_rgb)  # Set key 11 (index 11) to a different color
-                else:
-                    keys[11].set_led(0, 0, 0)  # Turn off key 11 LED
-            elif note == 25:  # C#0
-                if vel > 0:
-                    keys[10].set_led(*feedback_rgb)
-                else:
-                    keys[10].set_led(0, 0, 0)
-            elif note == 26:  # D0
-                if vel > 0:
-                    keys[9].set_led(*feedback_rgb)
-                else:
-                    keys[9].set_led(0, 0, 0)
-            elif note == 27:  # D#0
-                if vel > 0:
-                    keys[8].set_led(*feedback_rgb)
-                else:
-                    keys[8].set_led(0, 0, 0)
-            else:
-                # Handle other NoteOn messages normally for mapping
-                key_index = note - start_note
-                if 0 <= key_index < len(keys):
-                    if vel > 0:
-                        if key_index == 0:
-                            keys[key_index].set_led(*rec_rgb)
-                        elif key_index == 1:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 2:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 3:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 4:
-                            keys[key_index].set_led(*play_rgb)
-                        elif key_index == 5:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 6:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 7:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 8:
-                            keys[key_index].set_led(*ctl_rgb)
-                        elif key_index == 9:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 10:
-                            keys[key_index].set_led(*tap_rgb)
-                        elif key_index == 11:
-                            keys[key_index].set_led(*tap_rgb)
-                        else:
-                            keys[key_index].set_led(*rgb)
-                        key_led_states[key_index] = True
+                    if key_index == 0:
+                        keys[key_index].set_led(*rec_rgb)
+                    elif key_index == 1:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 2:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 3:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 4:
+                        keys[key_index].set_led(*play_rgb)
+                    elif key_index == 5:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 6:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 7:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 8:
+                        keys[key_index].set_led(*ctl_rgb)
+                    elif key_index == 9:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 10:
+                        keys[key_index].set_led(*tap_rgb)
+                    elif key_index == 11:
+                        keys[key_index].set_led(*tap_rgb)
                     else:
-                        keys[key_index].set_led(0, 0, 0)
-                        key_led_states[key_index] = False
+                        keys[key_index].set_led(*rgb)
+                    key_led_states[key_index] = True
+                else:
+                    keys[key_index].set_led(0, 0, 0)
+                    key_led_states[key_index] = False
 
         elif isinstance(msg, NoteOff):
             note = msg.note
@@ -139,21 +140,21 @@ def midi_feedback():
             # Use CC number to map to a key, or handle as desired
             if cc_number == 1:  # Example CC number for a specific action
                 if cc_value == 0:  # Threshold example, adjust as needed
-                    keys[11].set_led(*feedback_rgb)  # Example: Light up key 11
+                    keys[11].set_led(*feedback_rgb) 
                 elif cc_value == 4:
-                    keys[11].set_led(0, 0, 0)  # Turn off key 7
+                    keys[11].set_led(0, 0, 0)
                 elif cc_value == 1:
-                    keys[10].set_led(*feedback_rgb)  # Turn off key 7
+                    keys[10].set_led(*feedback_rgb)
                 elif cc_value == 5:
-                    keys[10].set_led(0, 0, 0)  # Turn off key 7
+                    keys[10].set_led(0, 0, 0)
                 elif cc_value == 2:
-                    keys[9].set_led(*feedback_rgb)  # Turn off key 7
+                    keys[9].set_led(*feedback_rgb)
                 elif cc_value == 6:
-                    keys[9].set_led(0, 0, 0)  # Turn off key 7
+                    keys[9].set_led(0, 0, 0)
                 elif cc_value == 3:
-                    keys[8].set_led(*feedback_rgb)  # Turn off key 7
+                    keys[8].set_led(*feedback_rgb)
                 elif cc_value == 7:
-                    keys[8].set_led(0, 0, 0)  # Turn off key 7
+                    keys[8].set_led(0, 0, 0)
             # Add more CC conditions as needed
 
 
@@ -162,4 +163,5 @@ while True:
     keybow.update()
     midi_feedback()
     time.sleep(0.01)  # Tiny sleep for smoother operation
+    
     
